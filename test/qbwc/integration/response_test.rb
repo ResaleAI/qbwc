772ddc5effb62d96756407cd2cbaec9a449c1573
Jackson Miller <jackson@resaleai.com>
2018-02-15 10:16:37 -0600

    REmoving a couple of debuggig messages

--- test/qbwc/integration/response_test.rb
+++ test/qbwc/integration/response_test.rb
@@ -29,7 +29,7 @@ class ResponseTest < ActionDispatch::IntegrationTest
   def _receive_responses(*responses)
 
     # Simulate controller authenticate
-    ticket_string = QBWC::ActiveRecord::Session.new(QBWC_USERNAME, COMPANY, nil, ACCOUNT_ID).ticket
+    ticket_string = QBWC::ActiveRecord::Session.new(QBWC_USERNAME, COMPANY, nil, ACCOUNT_IDZ).ticket
     assert_not_nil(ticket_string)
 
     session = QBWC::Session.new(nil, COMPANY, nil, ACCOUNT_ID)
